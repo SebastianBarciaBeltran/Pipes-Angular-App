bafd4bf44e07c334aecddfde0a4ccc3a065f204c
sebastianba <sebastianba@iadb.org>
2022-08-06 12:20:00 +0200

    structure of app are created and implement the primNg menuBar pending edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
 import { AppComponent } from './app.component';
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PrimeNgModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
